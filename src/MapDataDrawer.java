75affc0e399807c18c29067d7d39dfa4d99f6555
iDroid-User <steventejada6@gmail.com>
2026-05-16 22:45:21 -0700

    Implement drawMap() with corrected grayscale scaling and drawLowestElevPath()

--- src/MapDataDrawer.java
+++ src/MapDataDrawer.java
@@ -67,11 +67,10 @@ public class MapDataDrawer
   public void drawMap(Graphics g){
     int min = findMinValue(); // Darkest shade
     int max = findMaxValue(); // Brightest shade
-    int scale = (max + 255 - 1) / 255; // Scale elevation data to grayscale range (0-255) using ceiling division
 
     for (int row = 0; row < grid.length; row++) {
         for (int col = 0; col < grid[row].length; col++) {
-            int c = grid[row][col] / scale % 255; // Calculate grayscale value
+            int c = (int) Math.round((double)(grid[row][col] - min) / (max - min) * 255); // Calculate grayscale value
             g.setColor(new Color(c,c,c));
             g.fillRect(col,row,1,1); // Graphics are drawn in column-major order as 1x1 filled rectangles
         }
@@ -83,10 +82,50 @@ public class MapDataDrawer
    * Choose a forward step out of 3 possible forward locations, using greedy method described in assignment.
    * @return the total change in elevation traveled from West-to-East
    */
-  public int drawLowestElevPath(Graphics g, int row){
-    return -1;
+  public int drawLowestElevPath(Graphics g, int startRow) {
+      int row = startRow; // Keeps track of the current row
+      int totalElevChange = 0;
+      // Loops through every possible column across the map
+      for (int col = 0; col < grid[row].length; col++) {
+          // Starting from the given row, color it green
+          g.setColor(new Color(0,255,0));
+          g.fillRect(col,row,1,1);
+
+          if (col == grid[row].length - 1) return totalElevChange; // Guard clause
+          else {
+              int step = 0, change, smallestChange;
+              // Check the same row (the default) on the next column
+              smallestChange = Math.abs(grid[row][col] - grid[row][col + 1]);
+              // Check the previous row on the next column
+              if (row > 0) {
+                  change = Math.abs(grid[row][col] - grid[row - 1][col + 1]);
+                  if (smallestChange == change) step = 0; // If tied with the adjacent step, go forward
+                  else if (smallestChange > change) {
+                      smallestChange = change;
+                      step = -1; // Go forward-up
+                  }
+              }
+              // Check the next row on the next column
+              if (row < grid.length - 1) {
+                  change = Math.abs(grid[row][col] - grid[row + 1][col + 1]);
+                  if (smallestChange == change && step == 0) step = 0; // If tied with the adjacent step, go forward
+                  // If side steps tie
+                  else if (smallestChange == change) {
+                      Random rand = new Random();
+                      step = rand.nextBoolean() ? 1 : -1; // Choose randomly between fwd-up/fwd-down
+                  }
+                  else if (smallestChange > change) {
+                      smallestChange = change;
+                      step = 1; // Go forward-down
+                  }
+              }
+              totalElevChange += smallestChange;
+              row += step;
+          }
+      }
+      return totalElevChange;
   }
-  
+
   /**
    * @return the index of the starting row for the lowest-elevation-change path in the entire grid.
    */
